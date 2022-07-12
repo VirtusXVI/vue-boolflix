01ace3041eadb9e1739d744eb67ed22e869a4a7e
VirtusXVI <manuel.aveta2001@gmail.com>
2022-07-12 16:26:28 +0200

    checkpoint 1

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -2,9 +2,12 @@
   <div>
     <input v-model="filmName" type="text" name="filmname" id="filmname">
     <button @click="searchFilm">conferma</button>
-    <div class="films-container">
-        <div class="card">
-            <span>{{ this.searchedFilms[0][0].title }}</span>
+    <div v-if='searchedFilms' class="films-container">
+        <div v-for="film,index in searchedFilms[0]" :key="index" class="card">
+            <span>{{ this.searchedFilms[0][index].title }}</span>
+            <span>{{ this.searchedFilms[0][index].original_title }}</span>
+            <span>{{ this.searchedFilms[0][index].original_language }}</span>
+            <span>{{ this.searchedFilms[0][index].vote_average }}</span>
         </div>
     </div>
   </div>
@@ -25,7 +28,7 @@ export default {
             axios.get(`https://api.themoviedb.org/3/search/movie?api_key=d197ccf6d7b8c9f670d6f5e3440c730c&language=en-US&query=${this.filmName}&page=1&include_adult=false`)
             .then((response) => {
                 this.searchedFilms.push(response.data.results)
-                console.log(this.searchedFilms[0][0].title);
+                console.log(this.searchedFilms[0]);
             })
         }
     },
@@ -34,12 +37,14 @@ export default {
 
 <style>
     .films-container{
-        background-color: lightcoral;
         display: flex;
         justify-content: space-between;
     }
     .card{
         height: calc(100% / 2);
         width: calc(98% / 5);
+    }
+    .card span{
+        display: block;
     }
 </style>
