2b5dae267fa6976d6707ae777e85592d2323b3b6
VirtusXVI <manuel.aveta2001@gmail.com>
2022-07-13 15:37:06 +0200

    added series

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -2,12 +2,22 @@
   <div>
     <input v-model="filmName" type="text" name="filmname" id="filmname">
     <button @click="searchFilm">conferma</button>
+    <h2 :class="searchedFilms ? '' : 'titles'">Films</h2>
     <div v-if='searchedFilms' class="films-container">
         <div v-for="film,index in searchedFilms" :key="index" class="card">
             <span>{{ film.title }}</span>
             <span>{{ film.original_title }}</span>
-            <country-flag v-if="film.original_language" :country="!film.original_language ? `` : `${film.original_language}`" size='normal' rounded="true"/>
+            <country-flag v-if="film.original_language" :country="!film.original_language ? `` : `${film.original_language}`" size='normal'/>
             <span>{{ film.vote_average }}</span>
+        </div>
+    </div>
+    <h2 :class="searchedSeries ? '' : 'titles'">Series</h2>
+    <div v-if='searchedFilms' class="films-container">
+        <div v-for="series,index in searchedSeries" :key="index" class="card">
+            <span>{{ series.name }}</span>
+            <span>{{ series.name }}</span>
+            <country-flag v-if="series.original_language" :country="!series.original_language ? `` : `${series.original_language}`" size='normal'/>
+            <span>{{ series.vote_average }}</span>
         </div>
     </div>
   </div>
@@ -22,7 +32,8 @@
         data(){
             return {
                 filmName: "",
-                searchedFilms: [],
+                searchedFilms: null,
+                searchedSeries: null,
             }
         },
         components:{
@@ -35,9 +46,38 @@
                     this.searchedFilms = response.data.results
                     console.log(this.searchedFilms);
                     for(let i = 0;i < this.searchedFilms.length;i++){
-                        console.log(this.searchedFilms[i].original_language);
                         if(this.searchedFilms[i].original_language == "en"){
-                            this.searchedFilms[i].original_language = "gb"
+                             this.searchedFilms[i].original_language = "gb";
+                        } else if(this.searchedFilms[i].original_language == "ja"){
+                             this.searchedFilms[i].original_language = "jp";
+                        } else if(this.searchedFilms[i].original_language == "hi"){
+                             this.searchedFilms[i].original_language = "in";
+                        } else if(this.searchedFilms[i].original_language == 'cs'){
+                             this.searchedFilms[i].original_language = "cz";
+                        } else if(this.searchedFilms[i].original_language == "ko"){
+                             this.searchedFilms[i].original_language = "kr";
+                        } else if(this.searchedFilms[i].original_language == "sv"){
+                             this.searchedFilms[i].original_language = "ch";
+                        }
+                    }
+                })
+                axios.get(`https://api.themoviedb.org/3/search/tv?api_key=d197ccf6d7b8c9f670d6f5e3440c730c&language=en-US&query=${this.filmName}&page=1&include_adult=false`)
+                .then((response) => {
+                    this.searchedSeries = response.data.results
+                    console.log(this.searchedSeries);
+                    for(let i = 0;i < this.searchedSeries.length;i++){
+                        if(this.searchedSeries[i].original_language == "en"){
+                            this.searchedSeries[i].original_language = "gb";
+                        } else if(this.searchedSeries[i].original_language == "ja"){
+                             this.searchedSeries[i].original_language = "jp";
+                        } else if(this.searchedSeries[i].original_language == "hi"){
+                             this.searchedSeries[i].original_language = "in";
+                        } else if(this.searchedSeries[i].original_language == 'cs'){
+                             this.searchedSeries[i].original_language = "cz";
+                        } else if(this.searchedSeries[i].original_language == "ko"){
+                             this.searchedSeries[i].original_language = "kr";
+                        } else if(this.searchedSeries[i].original_language == "sv"){
+                             this.searchedSeries[i].original_language = "ch";
                         }
                     }
                 })
@@ -64,5 +104,8 @@
     }
     .card span{
         display: block;
+    }
+    .titles{
+        display: none;
     }
 </style>
