794cdaf771a3c2c69c1e8efc65ec0e01c085c3ea
VirtusXVI <manuel.aveta2001@gmail.com>
2022-07-13 15:02:01 +0200

    added flags to films

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -6,7 +6,7 @@
         <div v-for="film,index in searchedFilms" :key="index" class="card">
             <span>{{ film.title }}</span>
             <span>{{ film.original_title }}</span>
-            <span>{{ film.original_language }}</span>
+            <country-flag :country="!film.original_language ? `` : `${film.original_language}`" size='normal' rounded="true"/>
             <span>{{ film.vote_average }}</span>
         </div>
     </div>
@@ -14,25 +14,36 @@
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-    name:"MyInput",
-    data(){
-        return {
-            filmName: "",
-            searchedFilms: [],
-        }
-    },
-    methods:{
-        searchFilm(){
-            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=d197ccf6d7b8c9f670d6f5e3440c730c&language=en-US&query=${this.filmName}&page=1&include_adult=false`)
-            .then((response) => {
-                this.searchedFilms = response.data.results
-                console.log(this.searchedFilms);
-            })
-        }
-    },
-}
+    import axios from 'axios'
+    import CountryFlag from 'vue-country-flag'
+
+    export default {
+        name:"MyInput",
+        data(){
+            return {
+                filmName: "",
+                searchedFilms: [],
+            }
+        },
+        components:{
+            CountryFlag
+        },
+        methods:{
+            searchFilm(){
+                axios.get(`https://api.themoviedb.org/3/search/movie?api_key=d197ccf6d7b8c9f670d6f5e3440c730c&language=en-US&query=${this.filmName}&page=1&include_adult=false`)
+                .then((response) => {
+                    this.searchedFilms = response.data.results
+                    console.log(this.searchedFilms);
+                    for(let i = 0;i < this.searchedFilms.length;i++){
+                        console.log(this.searchedFilms[i].original_language);
+                        if(this.searchedFilms[i].original_language == "en"){
+                            this.searchedFilms[i].original_language = "gb"
+                        }
+                    }
+                })
+            }
+        },
+    }
 </script>
 
 <style>
@@ -40,11 +51,16 @@ export default {
         display: flex;
         justify-content: space-between;
         flex-wrap: wrap;
-        gap: 3rem;
+        width: 80%;
+        margin: 0 auto;
     }
     .card{
-        height: calc(100% / 2);
-        width: calc(98% / 5);
+        height: calc(100% / 4);
+        width: calc(90% / 5);
+        margin-top: 1rem;
+        border: 1px solid grey;
+        border-radius: 10px;
+        padding: 1rem;
     }
     .card span{
         display: block;
