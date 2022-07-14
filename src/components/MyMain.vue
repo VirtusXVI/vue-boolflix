965cae30043dbe59d41a3f27b43324f580f9df5f
VirtusXVI <manuel.aveta2001@gmail.com>
2022-07-14 18:32:12 +0200

    better graphics

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -3,7 +3,6 @@
     <div class="main-background">
         <div class="main-container">
             <div v-if='searchedFilms' class="films-container">
-                <div>
                     <div v-for="film,index in searchedFilms" :key="index" class="card">
                         <div class="covers">
                             <img v-if="film.poster_path" :src="`https://image.tmdb.org/t/p/w342${film.poster_path}`" alt="">
@@ -12,12 +11,10 @@
                         <div>
                             <span>{{ film.title }}</span>
                             <span>{{ film.original_title }}</span>
-                            <div><i  v-for="n in getVote(film.vote_average)" :key="'fullStar' + n"  class="fa-solid fa-star"></i><i v-for="n in 5 - getVote(film.vote_average)" :key="'emptyStar' + n" class="fa-regular fa-star"></i></div>
+                            <span><i  v-for="n in getVote(film.vote_average)" :key="'fullStar' + n"  class="fa-solid fa-star"></i><i v-for="n in 5 - getVote(film.vote_average)" :key="'emptyStar' + n" class="fa-regular fa-star"></i></span>
                             <country-flag v-if="film.original_language" :country="!film.original_language ? `` : `${film.original_language}`" size='normal'/>
                         </div>
                     </div>
-                </div>
-                <div>
                     <div v-for="series,index in searchedSeries" :key="index" class="card">
                         <div class="covers">
                             <img v-if="series.poster_path" :src="`https://image.tmdb.org/t/p/w342${series.poster_path}`" alt="">
@@ -30,7 +27,6 @@
                             <country-flag v-if="series.original_language" :country="!series.original_language ? `` : `${series.original_language}`" size='normal'/>
                         </div>
                     </div>
-                </div>
             </div>
         </div>
     </div>
@@ -70,20 +66,15 @@
     .films-container{
         display: flex;
         justify-content: space-between;
+        flex-wrap: wrap;
         width: 80%;
         margin: 0 auto;
-        max-height: 100%;
     }
     .card{
-        max-height: calc(100% / 5);
-        max-width: calc(90% / 1);
+        height: 50%;
+        width: calc(90% / 5);
         margin-top: 1rem;
         padding: 1rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        overflow-y: auto;
     }
     .covers{
         display: flex;
